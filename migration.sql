3628f865b1af6e484fc4dc355e97a86084ef31aa
nasa9084 <nasa9084@live.jp>
2016-11-30 00:11:14 +0900

    add: default charset to  table migration sql

--- migration.sql
+++ migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE {{prefix}}_users (
        login VARCHAR(64) PRIMARY KEY,
        passwd VARCHAR(512) NOT NULL
-);
+) DEFAULT CHARSET=utf8;
 
 CREATE TABLE {{prefix}}_articles (
        slug VARCHAR(64) PRIMARY KEY,
@@ -11,10 +11,10 @@ CREATE TABLE {{prefix}}_articles (
        status ENUM('draft', 'published') DEFAULT 'draft',
        created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
        published DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+) DEFAULT CHARSET=utf8;
 
 CREATE TABLE {{prefix}}_article_modified (
        slug VARCHAR(64) PRIMARY KEY,
        FOREIGN KEY (slug) REFERENCES {{prefix}}_articles (slug)
 
-);
+) DEFAULT CHARSET=utf8;
