0b4e817e607a4f7e2e1d41fcfc6f70b014171daf
nasa9084 <nasa9084@live.jp>
2016-12-04 23:55:04 +0900

    delete test code and add init code

---
+++ initialize.py
@@ -0,0 +1,23 @@
+import models
+import os
+
+import config as cfg
+
+# create image dir
+os.mkdir(cfg.img_save_path)
+
+# initialize database
+models.BaseModel.metadate.drop_all(models.engine)
+models.BaseModel.metadata.create_all(models.engine)
+
+# create hello article
+user = models.User.query().get('admin')
+new_article = {
+    'slug': 'hello',
+    'author': user.login,
+    'title': 'Hello',
+    'content': 'Hello, World',
+    'status': 'published'
+}
+models.session.add(models.Article(**new_article))
+models.session.commit()
