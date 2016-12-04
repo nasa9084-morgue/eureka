0559c862a8b413800966b9b3a0ca9db9e1de35d5
nasa9084 <nasa9084@live.jp>
2016-12-05 00:43:21 +0900

    fix: initialize script

--- initialize.py
+++ initialize.py
@@ -10,6 +10,11 @@ os.mkdir(cfg.img_save_path)
 models.BaseModel.metadata.drop_all(models.engine)
 models.BaseModel.metadata.create_all(models.engine)
 
+models.session.add(
+    models.User(**{'login': 'admin', 'password': 'PASSWORD'})
+)
+models.session.commit()
+
 # create hello article
 user = models.User.query().get('admin')
 new_article = {
