01215c6a0fbf2bbc96b7e00d0d33787455590973
nasa9084 <nasa9084@live.jp>
2016-12-09 03:58:59 +0900

    add: log dir making if not exists

--- usr/libexec/eureka/eureka.sh
+++ usr/libexec/eureka/eureka.sh
@@ -4,5 +4,9 @@ if [ ! -e /var/run/eureka ]; then
     mkdir /var/run/eureka
 fi
 
+if [ ! -e /var/log/eureka ]; then
+    mkdir /var/log/eureka
+fi
+
 cd /root/eureka
 /root/eureka/venv/bin/uwsgi uwsgi.ini
