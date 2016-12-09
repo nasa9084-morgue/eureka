ebf49aa5aba625694c2c25f963cfccf549646de3
nasa9084 <nasa9084@live.jp>
2016-12-09 17:26:51 +0900

    update: eureka.sh

--- usr/libexec/eureka/eureka.sh
+++ usr/libexec/eureka/eureka.sh
@@ -8,5 +8,10 @@ if [ ! -e /var/log/eureka ]; then
     mkdir /var/log/eureka
 fi
 
+if [ ! -e /root/eureka/app/static/css ]; then
+    mkdir /root/eureka/app/static/css
+fi
+
 cd /root/eureka
+/usr/local/bin/sass app/static/sass/style.scss app/static/css/style.css
 /root/eureka/venv/bin/uwsgi uwsgi.ini
